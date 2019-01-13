a94e00079a16f9ed10cad447e62e461177853b69
ArturoRR97 <arturorivas61@yahoo.com>
2019-01-12 23:33:01 -0600

    Spring y JS

---
+++ JavaScript/ejercicios/clase1.js
@@ -0,0 +1,22 @@
+//console.log("Hola Mundo JS!") //enviar a consola.
+var nombre
+
+nombre = 'Arturo'
+
+console.log('Hola '+ nombre)
+
+var nombreEnMayus = nombre.toUpperCase()
+
+var nombreEnMiyus = nombre.toLowerCase()
+
+var countNombre = nombre.length
+
+var primeraLetra = nombre.charAt(0)
+
+var apellido = 'Rivas'
+
+var concatenar = `${nombre} ${apellido}` // = nombre +''+ apellido
+
+var suma = "2 + 2 = " + (2+2)
+
+var ultimaLetra = nombre.charAt(nombre.length-1)
